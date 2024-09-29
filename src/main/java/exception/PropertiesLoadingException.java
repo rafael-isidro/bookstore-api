00850596d0b317c83fe5c2d196fd06090d682c30
Rafael Isidro <118776145+rafael-isidro@users.noreply.github.com>
2024-09-29 20:56:28 -0300

    Delete src directory

--- src/main/java/exception/PropertiesLoadingException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class PropertiesLoadingException extends RuntimeException {
-    public PropertiesLoadingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
